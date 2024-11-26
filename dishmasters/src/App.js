4010e6db9ccd6bfbadb05f57b4f52edd94fa9a95
WG32 TALDEA <ik012982i5@H012982.NET>
2024-11-26 09:39:38 +0100

    Upload full landing page

--- dishmasters/src/App.js
+++ dishmasters/src/App.js
@@ -1,29 +1,44 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './styles/App.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // This includes Popper.js
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-
+import Menu_Icon from './Components/Menu_Icon';
 import Sidebar from './Components/Sidebar';
 import Sidebar_Desktop from './Components/Sidebar_Desktop';
 import Header from './Components/Header';
 import SearchBar from './Components/SearchBar';
+import FormComponent from './Components/Form';
 import Carousel from './Components/Carousel';
 import Explore from './Components/Explore';
 import Footer from './Components/Footer';
 
-const App = () => (
-  <div className="container-fluid">
-    <Sidebar />
-    <Sidebar_Desktop />
-    <div className="col py-3 d-flex align-items-center flex-column" id="main">
-      <Header />
-      <SearchBar />
-      <Carousel />
-      <Explore/>
-      <Footer />
+const App = () => {
+  const [showForm, setShowForm] = useState(false); // State to manage form visibility
+
+  return (
+    <div className="container-fluid">
+      <div className="row flex-wrap">
+        <Menu_Icon />
+        {/* Sidebar Section */}
+        <div className="col-12 col-md-3 col-lg-2">
+          <Sidebar />
+          <Sidebar_Desktop setShowForm={setShowForm} /> {/* Pass setShowForm as prop */}
+        </div>
+
+        {/* Main Content Section */}
+        <div className="col-12 col-md-9 col-lg-10 py-3 d-flex flex-column align-items-center" id="main">
+          <Header />
+          <SearchBar />
+          <Carousel />
+          <Explore />
+          <Footer />
+        </div>
+      </div>
+
+      {/* Display Form Overlay */}
+      {showForm && <FormComponent setShowForm={setShowForm} />} {/* Show form conditionally */}
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
--- dishmasters/src/Components/Sidebar_Desktop.js
+++ dishmasters/src/Components/Sidebar_Desktop.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Sidebar = () => {
+const Sidebar = ({ setShowForm }) => {
 return (
     <div className="col-auto col-md-3 col-xl-2 d-none d-md-block greyscale-2" id="canvasMenu">
         <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 position-fixed" id="sidebar-menu">
@@ -19,101 +19,108 @@ return (
 
             {/* Menu */}
             <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-            {/* Home Link */}
-            <li className="nav-item">
-                <a href="#" className="nav-link align-middle px-0">
-                <i className="fs-3 bi-house"></i>
-                <span className="ms-1 d-none d-sm-inline">Home</span>
-                </a>
-            </li>
+                {/* Home Link */}
+                <li className="nav-item">
+                    <a href="#" className="nav-link align-middle px-0">
+                        <i className="fs-3 bi-house"></i>
+                        <span className="ms-1 d-none d-sm-inline">Home</span>
+                    </a>
+                </li>
 
-            {/* My Feed */}
-            <li>
-                <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                <i className="fs-3 bi bi-fire"></i>
-                <span className="ms-1 d-none d-sm-inline">My feed ▽</span>
-                </a>
-                <ul className="collapse nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                {/* My Feed */}
                 <li>
-                    <a href="myFeed.html" target="_blank" className="nav-link px-0">
-                    <span className="d-none d-sm-inline">Main Page</span>
+                    <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
+                        <i className="fs-3 bi bi-fire"></i>
+                        <span className="ms-1 d-none d-sm-inline">My feed ▽</span>
                     </a>
+                    <ul className="collapse nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                        <li>
+                            <a href="myFeed.html" target="_blank" className="nav-link px-0">
+                            <span className="d-none d-sm-inline">Main Page</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="nav-link px-0">
+                            <span className="d-none d-sm-inline">Dishy Challenge</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="nav-link px-0">
+                            <span className="d-none d-sm-inline">DishCuss</span>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
-                <li>
-                    <a href="#" className="nav-link px-0">
-                    <span className="d-none d-sm-inline">Dishy Challenge</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="nav-link px-0">
-                    <span className="d-none d-sm-inline">DishCuss</span>
-                    </a>
-                </li>
-                </ul>
-            </li>
 
-            {/* Dishylist */}
-            <li>
-                <a href="#submenu2" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                <i className="fs-3 bi bi-cloud-fog"></i>
-                <span className="ms-1 d-none d-sm-inline">Dishylist ▽</span>
-                </a>
-                <ul className="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
+                {/* Dishylist */}
                 <li>
-                    <a href="#" className="nav-link px-0">
-                    <span className="d-none d-sm-inline">Taste Trends</span>
+                    <a href="#submenu2" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
+                        <i className="fs-3 bi bi-cloud-fog"></i>
+                        <span className="ms-1 d-none d-sm-inline">Dishylist ▽</span>
                     </a>
+                    <ul className="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
+                        <li>
+                            <a href="#" className="nav-link px-0">
+                            <span className="d-none d-sm-inline">Taste Trends</span>
+                            </a>
+                        </li>
+                        <li className="w-100">
+                            <a href="seasonRecipes.html" className="nav-link px-0">
+                            <span className="d-none d-sm-inline">Season Recipes</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="nav-link px-0">
+                            <span className="d-none d-sm-inline">Food Spots</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="nav-link px-0">
+                            <span className="d-none d-sm-inline">World Dishes</span>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
-                <li className="w-100">
-                    <a href="seasonRecipes.html" className="nav-link px-0">
-                    <span className="d-none d-sm-inline">Season Recipes</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="nav-link px-0">
-                    <span className="d-none d-sm-inline">Food Spots</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="nav-link px-0">
-                    <span className="d-none d-sm-inline">World Dishes</span>
-                    </a>
-                </li>
-                </ul>
-            </li>
 
-            {/* About DishMasters */}
-            <li>
-                <a href="#submenu3" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                <i className="fs-3 bi bi-info-circle"></i>
-                <span className="ms-1 d-none d-sm-inline">About DishMasters ▽</span>
-                </a>
-                <ul className="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                <li className="w-100">
-                    <a href="#" className="nav-link px-0">
-                    <span className="d-none d-sm-inline">About Us</span>
-                    </a>
-                </li>
+                {/* About DishMasters */}
                 <li>
-                    <a href="#" className="nav-link px-0">
-                    <span className="d-none d-sm-inline">Contact Us</span>
+                    <a href="#submenu3" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
+                        <i className="fs-3 bi bi-info-circle"></i>
+                        <span className="ms-1 d-none d-sm-inline">About DishMasters ▽</span>
                     </a>
+                    <ul className="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
+                        <li className="w-100">
+                        <a href="#" className="nav-link px-0">
+                            <span className="d-none d-sm-inline">About Us</span>
+                        </a>
+                        </li>
+                        <li>
+                        <a
+                            href="#"
+                            className="nav-link px-0"
+                            onClick={(e) => {
+                            e.preventDefault(); // Prevent default anchor behavior
+                            setShowForm(true); // Show the form
+                            }}
+                        >
+                            <span className="d-none d-sm-inline">Contact Us</span>
+                        </a>
+                        </li>
+                        <li>
+                        <a href="#" className="nav-link px-0">
+                            <span className="d-none d-sm-inline">FAQ</span>
+                        </a>
+                        </li>
+                    </ul>
                 </li>
-                <li>
-                    <a href="#" className="nav-link px-0">
-                    <span className="d-none d-sm-inline">FAQ</span>
-                    </a>
-                </li>
-                </ul>
-            </li>
 
-            {/* Sign In */}
-            <li>
-                <a href="#" className="nav-link px-0 align-middle">
-                <i className="fs-3 bi bi-door-open-fill"></i>
-                <span className="ms-1 d-none d-sm-inline">Sign In</span>
-                </a>
-            </li>
+                {/* Sign In */}
+                <li>
+                    <a href="#" className="nav-link px-0 align-middle">
+                    <i className="fs-3 bi bi-door-open-fill"></i>
+                    <span className="ms-1 d-none d-sm-inline">Sign In</span>
+                    </a>
+                </li>
             </ul>
         </div>
     </div>
